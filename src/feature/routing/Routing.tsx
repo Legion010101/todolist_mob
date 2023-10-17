d12d02431fedbc76c623926a15fe395150072153
danilkorablev <korablev@e-krit.ru>
2023-10-17 23:56:28 +0300

    main- Роутинг

--- src/feature/routing/Routing.tsx
+++ src/feature/routing/Routing.tsx
@@ -1,70 +1,21 @@
-import React, {Dispatch, SetStateAction} from "react"
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import type { NativeStackScreenProps } from "@react-navigation/native-stack"
-import Main, {Todo} from "../../screens/Main/Index";
-import CompletedTodos from "../../screens/CompletedTodos/Index";
-import CreateTodo from "../../screens/CreateTodo/Index";
-import DeleteTodos from "../../screens/DeleteTodos/Index";
-import Profile from "../../screens/Profile/Index";
-import Settings from "../../screens/Settings/Index";
+import React from "react"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Auth from "../../screens/Auth/Index";
+import {NavigationContainer} from "@react-navigation/native";
+import MainRouting from "./routingStack/MainRouting";
+import {useSelector} from "react-redux";
+import {getCompletedTodos} from "../redux/selectors/todosSelector";
 
-
-const Stack = createNativeStackNavigator<RootStackParamList>()
+const Tab = createBottomTabNavigator()
 const Routing = () => {
+  const completedTodos = useSelector(getCompletedTodos)
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main" screenOptions={{
-        title: "список дел",
-        headerStyle: {
-          backgroundColor: "#1c1111"
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold"
-        },
-        headerBackTitle: "Back",
-        headerBackTitleVisible: true,
-        headerBackTitleStyle: {
-          fontSize: 20
-        }
-      }}
-      >
-        <Stack.Screen
-          name="Main" component={Main}
-        />
-        <Stack.Screen
-          name="CompletedTodos" component={CompletedTodos}
-        />
-        <Stack.Screen
-          name="CreateTodo" component={CreateTodo}
-        />
-        <Stack.Screen
-          name="DeleteTodos" component={DeleteTodos}
-        />
-        <Stack.Screen
-          name="Profile" component={Profile}
-        />
-        <Stack.Screen
-          name="Settings" component={Settings}
-        />
-      </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Главная" component={MainRouting} options={{ tabBarBadge: completedTodos.length }} />
+      <Tab.Screen name="Профиль" component={Auth} />
+    </Tab.Navigator>
     </NavigationContainer>
   )
 }
 export default Routing
-
-type RootStackParamList = {
-  Main: { post?: Todo }
-  CreateTodo: undefined
-  CompletedTodos: undefined
-  DeleteTodos: { Todos: Todo[], setTodosDeleted:Dispatch<SetStateAction<Todo[]>> }
-  Profile: undefined
-  Settings: undefined
-  Auth: undefined
-}
-export type PropsMain = NativeStackScreenProps<RootStackParamList, "Main">
-export type PropsCreateTodo = NativeStackScreenProps<RootStackParamList, "CreateTodo">
-export type PropsDeleteTodos = NativeStackScreenProps<RootStackParamList, "DeleteTodos">
-
-
---
+++ src/feature/routing/routingStack/MainRouting.tsx
@@ -0,0 +1,69 @@
+import React, {Dispatch, SetStateAction} from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import type { NativeStackScreenProps } from "@react-navigation/native-stack"
+import Main, {Todo} from "../../../screens/Main/Index";
+import CompletedTodos from "../../../screens/CompletedTodos/Index";
+import CreateTodo from "../../../screens/CreateTodo/Index";
+import DeleteTodos from "../../../screens/DeleteTodos/Index";
+import Profile from "../../../screens/Profile/Index";
+import Settings from "../../../screens/Settings/Index";
+
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+const MainRouting = () => {
+  return (
+      <Stack.Navigator initialRouteName="Main"
+                       screenOptions={{
+        title: "список дел",
+        headerStyle: {
+          backgroundColor: "#1c1111"
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold"
+        },
+        headerBackTitle: "Back",
+        headerBackTitleVisible: true,
+        headerBackTitleStyle: {
+          fontSize: 20
+        }
+      }}
+      >
+        <Stack.Screen
+          name="Main" component={Main}
+        />
+        <Stack.Screen
+          name="CompletedTodos" component={CompletedTodos} options={{title: "Выполненые дела"}}
+        />
+        <Stack.Screen
+          name="CreateTodo" component={CreateTodo} options={{title: "Добавить дело"}}
+        />
+        <Stack.Screen
+          name="DeleteTodos" component={DeleteTodos} options={{title: "Корзина"}}
+        />
+        <Stack.Screen
+          name="Profile" component={Profile}
+        />
+        <Stack.Screen
+          name="Settings" component={Settings}
+        />
+      </Stack.Navigator>
+  )
+}
+export default MainRouting
+
+type RootStackParamList = {
+  Main: { post?: Todo }
+  CreateTodo: undefined
+  CompletedTodos: undefined
+  DeleteTodos: { Todos: Todo[], setTodosDeleted:Dispatch<SetStateAction<Todo[]>> }
+  Profile: undefined
+  Settings: undefined
+  Auth: undefined
+}
+export type PropsMain = NativeStackScreenProps<RootStackParamList, "Main">
+export type PropsCreateTodo = NativeStackScreenProps<RootStackParamList, "CreateTodo">
+export type PropsDeleteTodos = NativeStackScreenProps<RootStackParamList, "DeleteTodos">
+
+
