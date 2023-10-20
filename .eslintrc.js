7be97243358f93d75122ddba0fa240fd91095d78
danilkorablev <korablev@e-krit.ru>
2023-10-20 23:08:27 +0300

    main - fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   root: true,
-  extends: ['@react-native', 'prettier'],
-  plugins: ['prettier'],
+  extends: ["@react-native", "prettier"],
+  plugins: ["prettier"],
   rules: {
-    'react-hooks/exhaustive-deps': 'off',
-    'prettier/prettier': [
-      'error',
+    "react-hooks/exhaustive-deps": "off",
+    "prettier/prettier": [
+      "error",
       {
-        endOfLine: 'lf',
-      },
-    ],
-  },
-};
+        endOfLine: "lf"
+      }
+    ]
+  }
+}
